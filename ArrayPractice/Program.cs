f46b168a2fc775802c9b43c4d9f83c282fd84b73
Alexander Burov <Alexander.Burov@GMR.local>
2023-02-18 16:09:04 -0800

    first commit

---
+++ ArrayPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ArrayPractice
+{
+    internal class Program
+    {
+        //Revese a string 
+        static void StringReverse(string strInput)
+        {
+            for (int i = strInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{strInput[i]}");
+            }
+        }
+
+        //Swap the two halves of an array :    1, 2, 3, 4, 5, 6, 7, 8 -> 5, 6, 7, 8, 1, 2, 3, 4
+        static void StringSwap(string strInput)
+        {
+            for(int i = 0, i <= strInput / 2)
+            {
+                Console.Write();
+            }
+        }
+    //Check if a string is palindrome
+    static void Main(string[] args)
+        {
+            string strInput = Console.ReadLine();
+            StringReverse(strInput);
+            Console.WriteLine("----------------------------------------------------------------");
+        }
+    }
+}
